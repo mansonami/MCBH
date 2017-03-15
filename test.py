434ed1232caa55dfcce4d1f7f89402df5a72e93c
aduya <845331767@qq.com>
2017-03-15 22:38:23 +0800

    更新bug

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-vipcode='333333' #vip卡号
-r=requests.get(url='http://127.0.0.1:5000/api/HfoMgEeRKSPUWuIiN0qvApj9nJ/vip/query_Counters/%s'%vipcode)
-ret=json.loads(r.text)
-print('  '.join(ret['zgname']))
-
-
